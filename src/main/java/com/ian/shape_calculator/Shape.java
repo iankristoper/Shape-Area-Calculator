af193b8321fd12b3d4e54cae930eb757e4f12355
iankristoper <iankristoper26@gmail.com>
2024-03-28 16:32:41 +0800

    UPDATE: super class features

---
+++ src/main/java/com/ian/shape_calculator/Shape.java
@@ -0,0 +1,12 @@
+package com.ian.shape_calculator;
+
+/**
+ *
+ * @author Ian Tagano
+ */
+
+//this is the superclass
+public abstract class Shape 
+{
+    public abstract double calculateArea(); //super method
+}
